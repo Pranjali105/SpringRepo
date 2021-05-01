7d79036490d9d26b2c8bb6289e775346b4b3d0bc
shree <shree@LAPTOP-11HGRT9P>
2021-05-01 17:17:12 +0530

    Commiting MyFirstSpringMVCProject from SpringRepo

---
+++ MyFirstSpringMVCProject/src/com/home/controller/SisController.java
@@ -0,0 +1,18 @@
+package com.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/sis")
+public class SisController {
+	
+	@ResponseBody
+	@RequestMapping("/doll")
+	public String getDoll()
+	{
+		return "Hey... Your Doll...";
+	}
+
+}
